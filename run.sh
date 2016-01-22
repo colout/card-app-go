9ac41dd0859028dd5e8a16c7eefa5d292a369df0
colout <choughto@gmail.com>
2016-01-22 14:26:44 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 # Stop golang container (will error if not started
 docker stop go
 
+# Delete it...
+docker rm go
+
 # Run start golang container
 docker run -d --name go -p 127.0.0.1:8080:8080 -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.3 /bin/bash -c "go get -t; go run -v main.go"
