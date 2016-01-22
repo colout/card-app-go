93ad547b5e3241ea99de913563a770bada75d656
colout <choughto@gmail.com>
2016-01-22 14:15:39 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Stop golang container (will error if not started
+docker stop go
+
+# Run start golang container
+docker run -d --name go -p 127.0.0.1:8080:8080 -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.3 /bin/bash -c "go get -t; go run -v main.go"
